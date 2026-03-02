d6cc5354e71cde61a5eaabf0404ac8f3f0d3135a
NAD <hanadjib70@gmail.com>
2026-03-02 23:55:50 +0100

    Fix CORS policy to allow localhost and Vercel

--- server/server.js
+++ server/server.js
@@ -9,7 +9,14 @@ connectDB();
 const app = express();
 
 app.use(express.json());
-app.use(cors());
+app.use(cors({
+  origin: [
+    'http://localhost:5173', // Pour tes tests en local
+    'https://portfolio-jjoih4fht-hanadjib70-8010s-projects.vercel.app/' // Remplace par ton VRAI lien Vercel !
+  ],
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  credentials: true
+}));
 
 
 app.get('/', (req, res) => {
